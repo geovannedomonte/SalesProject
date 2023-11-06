29542c0968c53ee83b5359ffdf6d1787ddcbea1f
Geovanne do Monte <geovannedomonte@gmail.com>
2023-11-06 20:01:20 -0300

    Construtor SalesRecord

--- SalesProject/SalesProject/Models/SallesRecord.cs
+++ SalesProject/SalesProject/Models/SallesRecord.cs
@@ -10,5 +10,18 @@ namespace SalesProject.Models
         public SaleStatus Status { get; set; }
 
         public Seller? Seller { get; set; }
+
+        public SallesRecord() 
+        {
+        }
+
+        public SallesRecord(int id, DateTime date, double amount, SaleStatus status, Seller? seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
     }
 }
