2ab848776829de40f2f7705f6d01f22dd4d5ca5e
Geovanne do Monte <geovannedomonte@gmail.com>
2023-11-06 18:58:04 -0300

    Association Seller * --- 1 Department
    Association Seller

--- SalesProject/SalesProject/Models/Seller.cs
+++ SalesProject/SalesProject/Models/Seller.cs
@@ -7,6 +7,8 @@
         public string? Email { get; set; }
         public DateTime birthDate { get; set; }
         public double baseSalary { get; set; }
+
+        public Department Department { get; set; }
     }
 
 }
