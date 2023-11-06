6ea861b5c77d985131e9ab84e649f50128197cb6
Geovanne do Monte <geovannedomonte@gmail.com>
2023-11-06 18:16:39 -0300

    Create class Seller

---
+++ SalesProject/SalesProject/Models/Seller.cs
@@ -0,0 +1,12 @@
+﻿namespace SalesProject.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public DateTime birthDate { get; set; }
+        public double baseSalary { get; set; }
+    }
+
+}
