e9f7ac6ec4d0251d0f89c4dbb214ed2fd97f1998
Geovanne do Monte <geovannedomonte@gmail.com>
2023-11-09 20:08:10 -0300

    Create em Seller

--- SalesProject/SalesProject/Controllers/SellersController.cs
+++ SalesProject/SalesProject/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesProject.Services;
+using SalesProject.Models;
 
 namespace SalesProject.Controllers
 {
@@ -17,5 +18,18 @@ namespace SalesProject.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
