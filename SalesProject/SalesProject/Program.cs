7ec5f229d1c7fae96b473c1e82d7d1e2852d329a
Geovanne do Monte <geovannedomonte@gmail.com>
2023-11-08 22:55:47 -0300

    SellerService

--- SalesProject/SalesProject/Program.cs
+++ SalesProject/SalesProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesProject.Data;
+using SalesProject.Services;
 
 
 
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<SalesProjectContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 
 
---
+++ SalesProject/SalesProject/Services/SellerService.cs
@@ -0,0 +1,21 @@
+﻿using SalesProject.Data;
+using SalesProject.Models;
+
+namespace SalesProject.Services
+{
+    public class SellerService
+    {
+
+        private readonly SalesProjectContext _context;
+
+        public SellerService(SalesProjectContext context) 
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll() 
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
